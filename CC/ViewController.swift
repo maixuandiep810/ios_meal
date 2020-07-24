05f26bf5814c9d9d8a36302339cdb5472227844d
maixuandiep810 <maixuandiep810@gmail.com>
2020-07-24 16:10:30 +0700

    View1 > Image

--- CC/ViewController.swift
+++ CC/ViewController.swift
@@ -8,13 +8,40 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITextFieldDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    // MARK: Properties
+    @IBOutlet weak var mealnameLabel: UILabel!
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var photoImageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        nameTextField.delegate = self
     }
-
-
+    
+    // MARK: Actions
+    @IBAction func setDefaultmealnameLabel(_ sender: Any) {
+        mealnameLabel.text = "Helu"
+    }
+    @IBAction func selectImageFromPhotoLibrary(_ sender: UITapGestureRecognizer) {
+        nameTextField.resignFirstResponder()
+    }
+    
+    
 }
 
+//MARK: UITextFieldDelegate
+extension ViewController {
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        mealnameLabel.text = textField.text
+    }
+    
+}
